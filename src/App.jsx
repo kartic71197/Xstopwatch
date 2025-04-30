dbafadb7c4f8db832329989c8e45383823d35d75
kartic71197 <karticmalhotra3289@gmail.com>
2025-04-30 11:24:39 +0530

    test1

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,37 @@
 import { useState, useRef } from "react";
 
 function App() {
-  const [seconds, setSeconds] = useState(0);
-  const [minutes, setMinutes] = useState(0);
+  const [totalSeconds, setTotalSeconds] = useState(0);
   const intervalRef = useRef(null);
 
   const startWatch = () => {
-    if (intervalRef.current !== null) return; 
+    if (intervalRef.current !== null) return;
 
     intervalRef.current = setInterval(() => {
-      setSeconds((prevSeconds) => {
-        if (prevSeconds === 59) {
-          setMinutes((prevMinutes) => prevMinutes + 1);
-          return 0;
-        }
-        return prevSeconds + 1;
-      });
+      setTotalSeconds((prev) => prev + 1);
     }, 1000);
   };
 
-  const resetWatch = () => {
-    clearInterval(intervalRef.current);
-    intervalRef.current = null;
-    setSeconds(0);
-    setMinutes(0);
-  };
   const stopWatch = () => {
     clearInterval(intervalRef.current);
     intervalRef.current = null;
   };
 
+  const resetWatch = () => {
+    stopWatch();
+    setTotalSeconds(0);
+  };
+
   const formatTime = (value) => value.toString().padStart(2, "0");
+
+  const minutes = Math.floor(totalSeconds / 60);
+  const seconds = totalSeconds % 60;
 
   return (
     <>
       <h1>Stopwatch</h1>
       <p>
-        Time {minutes}:{formatTime(seconds)}
+        Time: {(minutes)}:{formatTime(seconds)}
       </p>
       <button onClick={startWatch}>Start</button>
       <button onClick={stopWatch}>Stop</button>
